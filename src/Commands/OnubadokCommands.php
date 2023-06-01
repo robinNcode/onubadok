3ae9efe839f9a4f207842f77026dc66d94be5c65
robinNcode <msmrobin518@gmail.com>
2023-06-01 15:35:20 +0600

    Language code added on command

--- src/Commands/OnubadokCommands.php
+++ src/Commands/OnubadokCommands.php
@@ -6,8 +6,8 @@ use Illuminate\Filesystem\Filesystem;
 
 class OnubadokCommands extends Command
 {
-    protected $signature = 'onubadok:generate';
-    protected $description = 'Generate Bengali and English files using your custom command';
+    protected $signature = 'onubadok:generate {language?}';
+    protected $description = 'Generate Bengali and English files using onubadok command';
     private Filesystem $fileSystem;
 
     public function __construct()
@@ -23,31 +23,44 @@ class OnubadokCommands extends Command
     public function handle(): void
     {
         // generate a new instance of the Filesystem class
-        $filesystem = new Filesystem();
-        $base_folder = base_path() . '/lang';
+        $base_folder = base_path('lang');
 
-        // Check if the directory already exists
-        if ($filesystem->exists($base_folder)) {
-            $this->error('The directory already exists');
+        $language = $this->argument('language');
+
+        if ($language == null) {
+            $this->error('Please provide a language code');
+        }
+        else{
+            $folders = ['en', $language];
+            $this->generating($folders, $base_folder);
+        }
+    }
+
+    // generate the files in the app folder with the contents of the package files
+
+    /**
+     * @throws FileNotFoundException
+     */
+    protected function generating($folders, $base_folder): void
+    {
+        // Check if the directory already exists then override the base folder
+        if (!$this->generateController()) {
+            $this->error('Unable to generate the OnubadokController');
         } else {
-            if(!$this->generateController()){
-                $this->error('Unable to generate the OnubadokController');
-            }
-            else{
-                // Creating app/lang/en directory and generating files
-                $this->info('Generating files in the app/lang/en directory ...');
-                if ($this->generateFilesWithContents($base_folder, 'en')) {
-                    $this->info('The files were generated successfully in the app/lang/en directory');
+            foreach ($folders as $language) {
+                if ($this->fileSystem->exists($base_folder . '/' . $language)) {
+                    $this->info('The directory already exists');
+                    $this->info('Overriding the base folder');
+                    $base_folder = $base_folder . '/' . $language;
+                    break;
                 } else {
-                    $this->error('The files were not generated');
-                }
-
-                // Creating app/lang/bn directory and generating files
-                $this->info('Generating files in the app/lang/bn directory ...');
-                if ($this->generateFilesWithContents($base_folder, 'bn')) {
-                    $this->info('The files were generated successfully in the app/lang/bn directory');
-                } else {
-                    $this->error('The files were not generated');
+                    // Creating app/lang/en directory and generating files
+                    $this->info('Generating files in the app/lang/' . $language . ' directory ...');
+                    if ($this->generateFilesWithContents($base_folder, $language)) {
+                        $this->info('The files were generated successfully in the app/lang/' . $language . ' directory');
+                    } else {
+                        $this->error('The files were not generated');
+                    }
                 }
             }
         }
@@ -56,7 +69,8 @@ class OnubadokCommands extends Command
     /**
      * @throws FileNotFoundException
      */
-    public function generateFilesWithContents($base_folder, $language): bool
+    public
+    function generateFilesWithContents($base_folder, $language): bool
     {
         $package_folder = __DIR__ . '/../lang/' . $language . '/';
         $success = 0;
@@ -92,7 +106,8 @@ class OnubadokCommands extends Command
     /**
      * @throws FileNotFoundException
      */
-    public function generateController(): bool
+    public
+    function generateController(): bool
     {
         $app_folder = app_path() . '/Http/Controllers/';
         $package_folder = __DIR__ . '/../Controllers/';
